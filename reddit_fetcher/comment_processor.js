169e5a3e84581fafe2016287e982ede0b4acf3d5
Karsan Uthayakumar <karsan.uthayakumar@gmail.com>
2021-05-23 04:10:55 -0400

    edge case of before anchor being delete is handeled

---
+++ reddit_fetcher/comment_processor.js
@@ -0,0 +1,70 @@
+const { performance } = require('perf_hooks');
+const fs = require('fs');
+const readline = require('readline')
+/*
+    Fetch the comments given the before and process them.
+    If the comment is found with a stock name push the comment info to database.
+-----------------------------------------------------------------------
+    Important variables from the comment datatype received from the API request.
+    These values will be tracked in the database for the comment
+        - parent_id : this the the post that this comment is made on
+*/
+
+// process the CSV to populate the stock names 
+stock_names = {}
+stock_names_read = false 
+let t0;
+let t1;
+const result = {}
+function populate_stock_names(comments) {
+    const reader = readline.createInterface({
+        input: fs.createReadStream('stock_names.csv')
+    });
+    // this is an async function
+    reader.on('line', function (line) {
+        // process each line
+        parsed_data = line.split(',', 2)
+        stock_names[parsed_data[0]] = parsed_data[1]
+    }).on('close', function () {
+        // now we're done reading the file
+        stock_names_read = true
+        is_stock_name_in_comment(comments)
+    }).on('error', function (err) {
+        // handle errors here
+        console.log(err);
+    });
+}
+
+// check comments to see if stock name exits 
+function is_stock_name_in_comment(comments) {
+    comments.forEach((comment) => {
+        (Object.keys(stock_names)).forEach((key) => {
+            // if the stock name only a single char then do this  
+            if (key.length == 1) {
+                if (comment.data.body.includes(` $${key} `)) {
+                    result[key] = Object.keys(result).includes(key) ? result[key] + 1 : 1
+                }
+                // stock name is longer than one char
+            } else if (comment.data.body.includes(` ${key} `)||
+                 comment.data.body.includes(` $${key} `) 
+                ||comment.data.body.includes(` ${key}.`)) 
+                 {
+                result[key] = Object.keys(result).includes(key) ? result[key] + 1 : 1
+            }
+        })
+    })
+    console.log('result ', result)
+}
+
+// listen for parent's instructions
+process.on('message', (data) => {
+    if (stock_names_read === false) {
+        populate_stock_names(data.comments)
+        stock_names_read = true
+    } else {
+        is_stock_name_in_comment(data.comments)
+    }
+    console.log((data.id))
+    process.send(data.id)
+});
+ 
--- reddit_fetcher/index.js
+++ reddit_fetcher/index.js
@@ -1,8 +1,11 @@
-const os = require('os'),
+const os = require('os');
 const { fork } = require('child_process');
-const Reddit = require('reddit')
+const Reddit = require('./reddit_api_library_my_version.js')
+const { performance } = require('perf_hooks');
+const Queue = require('./queue_data_structure')
 
 // initialize reddit api client 
+let request_num = 0
 const reddit = new Reddit({
     username: 'paper_hands_service',
     password: 'paper_hasdfsdfnds_service',
@@ -11,20 +14,110 @@ const reddit = new Reddit({
     userAgent: 'MyApp/1.0.0 (http://example.com)'
 })
 
-// TODO get the data from API along with headers
-// TODO manage time requests, try and limit requests to once per seconds
-// TODO set up a process for processing the data that is coming in  
+// hard code to 4 processes for now
+const cpuCount = 4 // os.cpus().length
+const child_processes = {}
+// queue to keep track of which process needs to go next
+processor_queue = new Queue()
+// set up a process for processing the data that is coming in 
+function create_child_processes(cpuCount) {
+    for (let core = 0; core < cpuCount; core++) {
+        child_processes[core] = { id: core + "", process: fork('comment_processor.js') };
+        child_processes[core].process.on('error', (err) => {
+            // This will be called with err being an AbortError if the controller aborts
+            console.log(`Something went wrong with ${core}`)
+        });
+        child_processes[core].process.on('message', (id) => {
+            // call this when process is done with the batch of comments
+            processor_queue.enqueue(child_processes[id])
+        });
+        processor_queue.enqueue(child_processes[core])
+        //child_processes[core + ''].send( core );
+    }
+}
+
+// 
+
+
+// get the data from API along with headers
+let before = ""
+let before_is_working = false
+let t0;
+let t1;
 async function get_comments() {
     try {
-        const res = await reddit.get('/r/all/comments',
+        const opt = before && before_is_working ?
             {
+                before,
                 limit: 100,
-                after: 'gye0s9o'
-            })
-        console.log(res.res.header)
+                count: 0
+
+            } : {
+                limit: 100,
+                count: 0
+            }
+
+        // only fetch and call the processes if we have at least one available 
+        if (processor_queue.length >= 1) {
+            // call reddit api
+            let sub_reddits = "Investing+Stocks+Economics+StockMarket+Economy+GlobalMarkets+WallStreetBets+Options+Finance+Bitcoin+Dividends+Cryptocurrency+SecurityAnalysis+AlgoTrading+DayTrading+PennyStocks"
+            t0 = performance.now()
+            const res = await reddit.get(`/r/${sub_reddits}/comments`, opt)
+            t1 = performance.now()
+            console.log("The api call took " + (t1 - t0) + "milliseconds." + " We received: " + res.body.data.children.length + ' comments.')
+
+            // we have a case where our anchor before comment has been delete
+            // OR we didn't receive any comments on the last run 
+            let comments = res.body.data.children
+            if (comments.length > 0 && before !== ""
+                && comments[comments.length - 1].data.name.slice(3) < before.slice(3)) {
+                let index = 0
+                let id = before.slice(3)
+                while (comments[index].data.id > id) {
+                    index++
+                }
+                comments = comments.slice(0, index)
+                console.log(`New comments: ${comments.length}`)
+            }
+
+
+            // update before 
+            if (comments.length > 0) {
+                console.log("before: ", comments[0].data.name, res.body.data.before,
+                    " after: ", comments[comments.length - 1].data.name, res.body.data.after)
+                before = comments[0].data.name
+                before_is_working = true
+                // call the child process
+                const process_ref = processor_queue.dequeue()
+                process_ref.process.send({ comments, id: process_ref.id })
+                request_num++
+                console.log(request_num)
+                setTimeout(get_comments, 0)
+            } else {
+                before_is_working = false
+                request_num++
+                console.log(request_num)
+                setTimeout(get_comments, 0)
+            }
+        }
     } catch (e) {
         console.log(e)
     }
 }
+
+create_child_processes(cpuCount)
 get_comments()
- 
+
+
+/*
+console.log({
+                    "x-ratelimit-remaining": res.header["x-ratelimit-remaining"],
+                    "x-ratelimit-used": res.header["x-ratelimit-used"],
+                    "x-ratelimit-reset": res.header["x-ratelimit-reset"],
+                })
+
+console.log("before: ", comments[0].data.name, res.body.data.before,
+                    " after: ", comments[comments.length - 1].data.name, res.body.data.after)
+   console.log("The api call took " + (t1 - t0) + "milliseconds." + " We received: " + comments.length + ' comments.')
+            console.log("local before: ", before)
+*/
